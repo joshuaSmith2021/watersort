d6b0a16b3353797ae0275a0452d571473a1baacb
Joshua Smith <joshuasmith6556@gmail.com>
2023-06-30 13:37:39 -0700

    More work

--- watersort.py
+++ watersort.py
@@ -22,10 +22,13 @@ class Tube:
 
         return self.contents.pop()
 
-    def push(self, color: int):
+    def push(self, color: int, force: bool = False):
         # Ensure tube has space and the color matches the top color
         assert len(self.contents) < self.capacity
-        assert self.is_empty() or color == self.peek()
+
+        if not force:
+            assert self.is_empty() or color == self.peek()
+
         self.contents.append(color)
 
     def is_full(self) -> bool:
@@ -81,3 +84,74 @@ class Level:
             for dest in range(src + 1, len(self.tubes)):
                 if move_possible(self.tubes[src], self.tubes[dest]):
                     yield (src, dest)
+
+    def is_complete(self) -> bool:
+        return all(x.is_complete() for x in self.tubes)
+
+
+class Move:
+    src: int
+    dest: int
+    n: int
+
+    def __init__(self, src: int, dest: int, n: int) -> None:
+        self.src = src
+        self.dest = dest
+        self.n = n
+
+    def __str__(self) -> str:
+        return f'{self.src} -> {self.dest}, n = {self.n}'
+
+    __repr__ = __str__
+
+
+def undo_move(level: Level, move: Move):
+    src = move.src
+    dest = move.dest
+    n = move.n
+
+    tubes = level.tubes
+
+    for _ in range(n):
+        tubes[src].push(tubes[dest].pop(), force=True)
+
+
+def solve_level(level: Level, history: List[Move]):
+    print(history)
+
+    for src, dest in level.possible_moves():
+        n = tube_dump(level.tubes[src], level.tubes[dest])
+        history.append(Move(src, dest, n))
+
+        solve_level(level, history)
+
+        undo_move(level, history.pop())
+
+
+if __name__ == '__main__':
+    PURPLE = 0
+    PINK = 1
+    LIME = 2
+    GREEN = 3
+    BLUE = 4
+    GRAY = 5
+    RED = 6
+    ORANGE = 7
+    SKY = 8
+
+    # Level 97
+    level = Level([
+        Tube(4, [GREEN, LIME, PINK, PURPLE]),
+        Tube(4, [GRAY, GRAY, PINK, BLUE]),
+        Tube(4, [GREEN, ORANGE, PURPLE, RED]),
+        Tube(4, [GRAY, BLUE, SKY, BLUE]),
+        Tube(4, [BLUE, SKY, LIME, RED]),
+        Tube(4, [PINK, RED, ORANGE, SKY]),
+        Tube(4, [SKY, LIME, ORANGE, RED]),
+        Tube(4, [LIME, PINK, GRAY, GREEN]),
+        Tube(4, [PURPLE, GREEN, PURPLE, ORANGE]),
+        Tube(4, []),
+        Tube(4, [])
+    ])
+
+    solve_level(level, [])
